53a9a54e8abbb12c68003344a3fc41e3f868c9fd
Isaac Cohen <isaaclandoncohen@gmail.com>
2019-05-29 09:16:00 -0600

    adding more

---
+++ Assets/PlaceParticlesOnDynamicMesh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceParticlesOnDynamicMesh : LifeForm {
+
+  public Life life;
+  public Form verts;
+  public Form skeleton;
+
+  public override void Create(){
+    Cycles.Insert(0,verts);
+    Cycles.Insert(1,life);
+  }
+  public override void Bind(){
+
+    life.BindPrimaryForm("_VertBuffer", verts);
+    life.BindForm( "_SkeletonBuffer" , skeleton );
+
+  }
+
+  public  void Set(){
+    life.Live();
+  }
+
+}
