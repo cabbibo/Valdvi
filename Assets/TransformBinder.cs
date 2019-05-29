56a8f5effa668883ca01b1c61e60048c9f671117
Isaac Cohen <isaaclandoncohen@gmail.com>
2019-05-29 10:52:57 -0600

    Adding more

---
+++ Assets/TransformBinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformBinder : Cycle {
+
+  public Life toBind;
+  public Transform t;
+
+
+  public float[] transformFloats;
+
+
+
+  // Use this for initialization
+  public override void Bind() {
+    toBind.BindAttribute( "_Transform" , "transformFloats" , this );
+  }
+
+  public override void WhileLiving( float v ){
+    transformFloats = HELP.GetMatrixFloats( t.localToWorldMatrix );
+  }
+  
+
+
+}
